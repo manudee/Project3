809bb58fcd81ceb9ec14862e2d1a6a6aed8ac86e
abhijit2908 <abhijitkghosh29@gmail.com>
2018-02-07 22:10:42 -0500

    fully functioning UI fro createRequest

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -6,17 +6,45 @@ import Container from '../components/Container/Container';
 
 class LoginPage extends Component{
 
+    state={
+        username:"",
+        password:""
+    }    
+
+    handleInputChange = event => {
+        const{name,value} = event.target;
+        this.setState({
+          [name]:value
+        })
+       };
+
+       handleFormSubmit = event => {
+        event.preventDefault();
+
+        var username=this.state.username;
+        var password=this.state.password;
+
+        var loginInfo = {}
+        loginInfo.username = username;
+        loginInfo.password = password;
+
+        console.log(loginInfo);
+
+       }
+
+
+
     render(){
         return(
             <div>
                 <Container>
         <form>
       <Title>Login</Title>
-     <Input name="username" placeholder="username"/>
+     <Input name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange} />
      <Title>Password</Title>
-     <Input name="password" placeholder="password"/>
+     <Input name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange} />
     </form>
-    <FormButton>Login</FormButton>
+    <FormButton onClick={this.handleFormSubmit}>Login</FormButton>
     </Container>
                 </div>
         )
