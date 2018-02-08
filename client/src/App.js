779dd97f3b022433fc1413ac5aa2e55d7b4500b4
Manasi <manudee@gmail.com>
2018-02-07 23:03:05 -0500

    Added Manager Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Jumbotron from './components/Jumbotron/Jumbotron';
 import LoginPage from './Pages/LoginPage.js'
 import CreateRequest from './Pages/CreateRequest';
 import UserTable from './Pages/UserTable';
+import ManagerTable from './Pages/ManagerTable';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
@@ -16,6 +16,7 @@ const App = () =>
         <Route exact path="/" component={LoginPage} />
         <Route exact path="/createrequest" component={CreateRequest} />
         <Route path="/user" exact component={UserTable} />
+        <Route path="/manager" exact component={ManagerTable} />
       </Switch>
       </div>
   </Router>    
---
+++ client/src/Pages/ManagerTable.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
+import { SaveBtn } from "../components/Button/SaveBtn.js";
+
+class ManagerTable extends Component {
+    render() {
+        const data = [{
+            Reqid: '101010',
+            userName: 'Manasi',
+            equipment: 'Laptop',
+            Description: 'This is a laptop request',
+            Quantity: 10,
+            Justification: 'This is a laptop request for Manasi'
+        },
+        {
+            Reqid: '202020',
+            userName: 'Abhijit',
+            equipment: 'Mouse',
+            Description: 'This is a Mouse request',
+            Quantity: 10,
+            Justification: 'This is a laptop request for Abhijit'
+        }, {
+            Reqid: '202020',
+            userName: 'Brian',
+            equipment: 'PC',
+            Description: 'This is a PC request',
+            Quantity: 10,
+            Justification: 'This is a PC request for Brian'
+        },
+
+        {
+            Reqid: '202020',
+            userName: 'Ted',
+            equipment: 'Monitor',
+            Description: 'This is a Monitor request',
+            Quantity: 10,
+            Justification: 'This is a Monitor request for Ted'
+        }
+        ]
+
+        const columns = [{
+            Header: 'Reqid',
+            accessor: 'Reqid'
+
+
+        }, {
+            Header: 'userName',
+            accessor: 'userName',
+          
+        },
+        {
+            Header: 'equipment',
+            accessor: 'equipment',
+          
+        }, {
+            Header: 'Description', // Custom header components!
+            accessor: 'Description'
+        },
+        {
+            Header: 'Quantity', // Custom header components!
+            accessor: 'Quantity'
+        },
+        {
+            Header: 'Justification', // Custom header components!
+            accessor: 'Justification'
+        },
+        {
+            Header: 'Action',
+            accessor: 'Action',
+            Cell: () => (<SaveBtn value='Approve' />)
+        }]
+
+        return (
+            <div>
+                <div className="row">
+                    <SaveBtn value='Create Request' />
+                    <SaveBtn value='Create User' />
+                    <SaveBtn value='All Users' />
+                    <SaveBtn value='All Equipments' />
+                </div>
+                <ReactTable
+                    data={data}
+                    columns={columns}
+                    defaultPageSize={10}
+                    className="-striped -highlight text-center" /></div>)
+    }
+}
+
+export default ManagerTable;
