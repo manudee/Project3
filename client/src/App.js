c545a5a876d8cc20141c8831fd33daa7220bfe25
abhijit2908 <abhijitkghosh29@gmail.com>
2018-02-07 23:08:58 -0500

    equipment table

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Jumbotron from './components/Jumbotron/Jumbotron';
 import LoginPage from './Pages/LoginPage.js'
 import CreateRequest from './Pages/CreateRequest';
 import UserTable from './Pages/UserTable';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import EquipmentTable from './Pages/EquipmentTable';
 
 
 const App = () =>
@@ -16,6 +16,7 @@ const App = () =>
         <Route exact path="/" component={LoginPage} />
         <Route exact path="/createrequest" component={CreateRequest} />
         <Route path="/user" exact component={UserTable} />
+        <Route path="/equipment" exact component={EquipmentTable} />
       </Switch>
       </div>
   </Router>    
