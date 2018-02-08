e890330eef49c95cc5dd67fcd33d433e9f5b7c8f
Manasi <manudee@gmail.com>
2018-02-07 22:21:14 -0500

    Added user route

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import Jumbotron from './components/Jumbotron/Jumbotron';
 import LoginPage from './Pages/LoginPage.js'
 import CreateRequest from './Pages/CreateRequest';
-
-
+import UserTable from './Pages/UserTable';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () =>
-  
+<Router>
       <div className="App">
-      <Jumbotron/>
-      <CreateRequest/>
+            <Jumbotron/>
+            <Switch>
+                  
+                  <Route path="/user" exact component={UserTable} />
+            </Switch>
+            
+            {/* <Route path='/user' component={UserTable}/> */}
+            <UserTable />
       </div>
-
+</Router>
 export default App;
 
--- client/src/components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const SaveBtn = (props) => (
-   
-        <button {...props} className = 'btn btn-primary' type="submit">{props.value}</button>
-       
-)
